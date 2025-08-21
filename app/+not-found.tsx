13c4e4824d1b99c86a7633d12d0a9ea2f1c31558
unknown <vinicius_24749@ETEC.FURLAN>
2025-08-20 22:32:33 -0300

    atualização

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -9,9 +9,9 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
+        <ThemedText type="title">Essa não é a tela que você procura.</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link">Volte para a tela inicial!</ThemedText>
         </Link>
       </ThemedView>
     </>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  useEffect(()=>{
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -18,12 +24,32 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    <GestureHandlerRootView>
+      <ThemeProvider value={DarkTheme}>
+        
+        <Stack>
+
+          <Stack.Screen 
+            name="index" options={{ headerShown: false }} 
+          />
+
+          <Stack.Screen 
+            name="permissions" 
+            options={{presentation: "modal", headerShown: true}}
+          />
+
+          <Stack.Screen
+            name="media"
+            options={{presentation: "modal", headerShown: false}}
+          />
+
+          <Stack.Screen
+            name="+not-found"
+            options={{presentation: "modal"}}
+          />
+        
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
