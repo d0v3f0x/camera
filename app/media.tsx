35471468fd3b3844405405ded1ec1f11c7a4c9c0
d0v3f0x <viniciuspiresrodrigues@gmail.com>
2025-09-03 22:26:34 -0300

    main

--- app/media.tsx
+++ app/media.tsx
@@ -1,4 +1,61 @@
+import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 
+import { Alert, Image, StyleSheet } from "react-native";
 
+import { ThemedText } from "@/components/ThemedText"
+import { ThemedView } from "@/components/ThemedView"
 
+import ObscuraButton from "@/components/ObscuraButton"
 
+import { saveToLibraryAsync } from "expo-media-library";
+
+export default function MediaScreen(){
+
+    const { media, type } = useLocalSearchParams();
+
+    const router = useRouter();
+
+    console.log (media, type);
+    return(
+        <ThemedView style = {styles.container}>
+            {
+                type === "photo" ? (
+                    <Image
+                        source = {{url:`file://${media}`}}
+                        style = {{width: "100%", height: "80%", resizeMode: "contain" }}
+                    />
+                ) : null
+            }
+
+            
+            <ObscuraButton
+                title = "Salvar na galeria"
+                containerStyle = {{ alignself: "center" }}
+                onPress = {async () => {
+                    saveToLibraryAsync(media as string);
+                    Alert.alert("Salvo na galeria!")
+                    router.back();
+                }}
+            />
+
+            <Link href = "/" style = {styles.link}>
+                <ThemedText type = "link"> Deletar e voltar a tela inicial </ThemedText>
+            </Link>
+            
+            
+            
+        </ThemedView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+        alignSelf: "center"
+    },
+})
